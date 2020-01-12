31448987cf2bde0db7bdca683951d036f4b2f692
Ivan Zou <izou3@gatech.edu>
2020-01-11 23:10:03 -0500

    Added Mistake Counter for Each Typing Session

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-'use strict'; 
+'use strict';
 
-const textToCopy = document.querySelector(".requiredText p"); 
-const counter = document.querySelector(".counter"); 
-const submitButton = document.querySelector(".operations .start"); 
-const clock = document.querySelector(".timer a"); 
-const textarea = document.querySelector(".textbox textarea"); 
-var mistakeCounter = document.querySelector(".timer p"); 
+const textToCopy = document.querySelector(".requiredText p");
+const counter = document.querySelector(".counter");
+const submitButton = document.querySelector(".operations .start");
+const clock = document.querySelector(".timer a");
+const textarea = document.querySelector(".textbox textarea");
+var mistakeCounter = document.querySelector(".timer p");
 var timerRunning = false;
-var interval; 
-var timer = ["00", "00", "00", "00"]; 
-var numMistake = 0; 
+var interval;
+var timer = ["00", "00", "00", "00"];
+var numMistake = 0;
 
 submitButton.addEventListener("click", function() {
 
     if(submitButton.innerHTML == "Start") {
         let buttonDisable = document.createAttribute("disabled");
-        buttonDisable.value = ""; 
+        buttonDisable.value = "";
         submitButton.setAttributeNode(buttonDisable); 
     }
 
@@ -30,37 +30,37 @@ submitButton.addEventListener("click", function() {
     if(submitButton.innerHTML=="Start") {
         const countdown = (async() => {
             for(let i = 3; i > -1; i--) {
-                await count(i);  
-            } 
-    
+                await count(i);
+            }
+
             counter.innerHTML = "Go!";
-            textarea.removeAttribute("disabled");  
-            start(); 
-        })();  
+            textarea.removeAttribute("disabled");
+            start();
+        })();
     } else {
-        reset(); 
+        reset();
     }
 
-}, false); 
+}, false);
 
 function start() {
     if (!timerRunning) {
-        timerRunning = true; 
-        submitButton.innerHTML = "Reset"; 
-        counter.classList.add("hidden"); 
-        counter.innerHTML = "3";  
-        submitButton.removeAttribute("disabled");  
+        timerRunning = true;
+        submitButton.innerHTML = "Reset";
+        counter.classList.add("hidden");
+        counter.innerHTML = "3";
+        submitButton.removeAttribute("disabled");
         interval = setInterval(runTimer, 10);
     }
 }
 
 
 function runTimer(){
-    let currentTime = `${leadingZero(timer[0])}:${leadingZero(timer[1])}:${leadingZero(timer[2])}`; 
+    let currentTime = `${leadingZero(timer[0])}:${leadingZero(timer[1])}:${leadingZero(timer[2])}`;
     clock.innerHTML = currentTime;
-    timer[3]++; 
+    timer[3]++;
 
-    timer[0] = Math.floor((timer[3]/100)/60); 
+    timer[0] = Math.floor((timer[3]/100)/60);
     timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
     timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 
@@ -76,38 +76,37 @@ function leadingZero(time) {
 
 function reset() {
     let textDisable = document.createAttribute("disabled");
-    textDisable.value = ""; 
-    textarea.setAttributeNode(textDisable); 
+    textDisable.value = "";
+    textarea.setAttributeNode(textDisable);
 
-    clearInterval(interval); 
-    interval = null; 
+    clearInterval(interval);
+    interval = null;
     timer = [0,0,0,0];
     timerRunning = false;
-    numMistake = 0; 
-    mistakeCounter.innerHTML = 0; ; 
+    numMistake = 0;
+    mistakeCounter.innerHTML = 0; ;
 
-    textarea.value = ""; 
-    clock.innerHTML = "00:00:00"; 
+    textarea.value = "";
+    clock.innerHTML = "00:00:00";
     submitButton.innerHTML = "Start";
-    textarea.style.borderColor = "black"; 
-    counter.classList.add("hidden");  
+    textarea.style.borderColor = "black";
+    counter.classList.add("hidden");
 }
 
 textarea.addEventListener("keyup", function() {
-    let textEntered = textarea.value; 
-    let currentTextMatch = textToCopy.innerHTML.substring(0, textEntered.length); 
-     
+    let textEntered = textarea.value;
+    let currentTextMatch = textToCopy.innerHTML.substring(0, textEntered.length);
+
     if(textEntered == textToCopy.innerHTML) {
-        clearInterval(interval); 
-        timerRunning = true; 
+        clearInterval(interval);
+        timerRunning = true;
     } else {
         if(textEntered == currentTextMatch) {
-            textarea.style.borderColor = "#00b300"; 
+            textarea.style.borderColor = "#00b300";
         } else {
-            textarea.style.borderColor = "#F05837"; 
-            numMistake ++; 
+            textarea.style.borderColor = "#F05837";
+            numMistake ++;
         }
     }
-    
-}, false); 
 
+}, false);
